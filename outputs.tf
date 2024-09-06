26a67342294bb57ad336995dec0d83784df889dc
pkalsh <pkalsh@lgupluspartners.co.kr>
2024-09-06 16:44:49 +0900

    feat: apply multiple output url, ip to list

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
 output "catapp_url" {
-  value = "http://${aws_eip.hashicat.public_dns}"
+  value = formatlist("http://%s", aws_eip.hashicat.*.public_dns)
 }
 
 output "catapp_ip" {
-  value = "http://${aws_eip.hashicat.public_ip}"
+  value = formatlist("http://%s", aws_eip.hashicat.*.public_ip)
 }
